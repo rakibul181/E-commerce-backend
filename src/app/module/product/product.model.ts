2258c1b213d3473609a67e0c518e58c9062edca0
Rakibul Hasan <rakibulhasan0641@gmail.com>
2024-05-22 18:19:07 +0600

    Student interface and Schema and model created

---
+++ src/app/module/product/product.model.ts
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+import { TProduct } from "./product.interface";
+
+const productSchema = new Schema<TProduct>({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: [String],
+        default: []
+    },
+    variants: {
+        type: [{
+            type: {
+                type: String,
+                required: true
+            },
+            value: {
+                type: String,
+                required: true
+            }
+        }],
+        default: []
+    },
+    inventory: {
+        quantity: {
+            type: Number,
+            required: true
+        },
+        inStock: {
+            type: Boolean,
+            required: true
+        }
+    }
+})
+
+export const Product = mongoose.model('Product', productSchema)
